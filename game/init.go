c061e7006f1d053906801daa85c99ff3a928ccb5
E247922F <E247922F>
2025-01-21 00:46:28 +0100

    extension 6.6(2)

--- game/init.go
+++ game/init.go
@@ -9,26 +9,38 @@ import (
 // faire attention à l'ordre des initialisation car elles
 // pourraient dépendre les unes des autres.
 func (g *Game) Init() {
-
+	g.floor.Init()
 	if configuration.Global.CameraMode != 2 {
-		g.floor.Init()
+
 		g.character.Init(g.floor.GetWidth(), g.floor.GetHeight())
 		g.camera.Init(g.character.X, g.character.Y)
 	}
 	if configuration.Global.CameraMode == 2 {
-		g.floor.Init()
-		g.floor.GenerateRandomFloor(configuration.Global.NumTileX*2, configuration.Global.NumTileY*2, 6)
 
-		// Initialisation du personnage après le terrain pour obtenir les dimensions correctes
-		g.character.Init(g.floor.GetWidth(), g.floor.GetHeight())
+		if configuration.Global.EnableRandomTerrain {
+			g.floor.GenerateRandomFloor(configuration.Global.NumTileX*2, configuration.Global.NumTileY*2, 6)
 
-		// Initialisation de la caméra après le personnage pour obtenir les coordonnées initiales correctes
-		g.camera = camera.Camera{
-			X:           g.character.X,
-			Y:           g.character.Y,
-			FullContent: g.floor,
+			// Initialisation du personnage après le terrain pour obtenir les dimensions correctes
+			g.character.Init(g.floor.GetWidth(), g.floor.GetHeight())
+
+			// Initialisation de la caméra après le personnage pour obtenir les coordonnées initiales correctes
+			g.camera = camera.Camera{
+				X:           g.character.X,
+				Y:           g.character.Y,
+				FullContent: g.floor,
+			}
+		} else {
+			// Initialisation du personnage après le terrain pour obtenir les dimensions correctes
+			g.character.Init(g.floor.GetWidth(), g.floor.GetHeight())
+
+			// Initialisation de la caméra après le personnage pour obtenir les coordonnées initiales correctes
+			g.camera = camera.Camera{
+				X:           g.character.X,
+				Y:           g.character.Y,
+				FullContent: g.floor,
+			}
+
 		}
-
 	}
 
 }
